4e99a4b30cd0ba5a2ca009eb30b6a705aa547770
Petr Janoušek <petrjanousek.net@gmail.com>
2023-06-08 23:08:28 +0200

    Adding source

---
+++ MPEGTS/ProgramAssociation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MPEGTS
+{
+    public class ProgramAssociation
+    {
+        public int ProgramNumber { get;set; }
+        public int ProgramMapPID { get; set; }
+    }
+}
---
+++ MPEGTS/SubtitlingInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MPEGTS
+{
+    public class SubtitlingInfo
+    {
+        public string LanguageCode { get; set; }
+
+        public byte SubtitlingType { get; set; }
+        public int CompositionPageId { get; set; }
+        public int AncillaryPageId { get; set; }
+    }
+}
